1ee88cee0273560dc9fb44c9738428f27bce15f3
= <snitinbharadwaj@gmail.com>
2021-07-07 09:52:05 +0530

    AssignmentComplete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.symbols = ['GOOG','FB','BLKB','JKHY','TXN', 'APPL'];
+    this.state = { _data: {} };
+  }
+
+  componentDidMount = () => {
+    fetch('api?symbols='  + this.symbols.join(',')).then(data => this.setState({_data: data}))
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <div className="marquee">
+              <span>
+                {
+                  this.symbols.map(Symbol => {
+                    return (Symbol + ':' + 
+                              ( (this.state['_data'] && this.state['_data'][Symbol] &&this.state['_data'][Symbol].price.currencySymbol) || ' ') + 
+                              ((this.state['_data'] && this.state['_data'][Symbol] && this.state['_data'][Symbol].price.regularMarketPrice) || 'Fetching Data '));
+                  })
+                }
+              </span>
+            </div>
+          </header>
+        </div>
+      );
+    }
 }
 
 export default App;
