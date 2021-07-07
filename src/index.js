b9947653517c7c1de43ef9a71e5d1f0d2c6984ce
swadeshimart87 <72510759+swadeshimart87@users.noreply.github.com>
2021-07-07 10:36:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === "staging"{
+      app.use(express.static("build"));
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+      });
+    }
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
